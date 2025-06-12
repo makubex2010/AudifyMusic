a01779ddc9c9f32f4825e77eb74f9fcc82d15901
Nickhil <teamstormxd@gmail.com>
2025-06-12 14:41:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/StormBeatz/AudifyMusic",
+    "https://github.com/Nikchil/AudifyMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
